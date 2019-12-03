0ed10789fc6e44ab42e3c212c785c0eeb662c6d8
stanisz93 <stanisz199@gmail.com>
2019-12-03 21:35:04 +0100

    Ready app of ECG
    Add code from app

---
+++ moc_mainwindow.cpp
@@ -0,0 +1,204 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.5.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../ECG/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#include <QtCore/QVector>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.5.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[31];
+    char stringdata0[426];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 21), // "on_pushButton_clicked"
+QT_MOC_LITERAL(2, 33, 0), // ""
+QT_MOC_LITERAL(3, 34, 23), // "on_pushButton_2_clicked"
+QT_MOC_LITERAL(4, 58, 32), // "on_horizontalSlider_valueChanged"
+QT_MOC_LITERAL(5, 91, 5), // "value"
+QT_MOC_LITERAL(6, 97, 34), // "on_horizontalSlider_2_valueCh..."
+QT_MOC_LITERAL(7, 132, 12), // "xAxisChanged"
+QT_MOC_LITERAL(8, 145, 8), // "QCPRange"
+QT_MOC_LITERAL(9, 154, 5), // "range"
+QT_MOC_LITERAL(10, 160, 13), // "xAxisChanged2"
+QT_MOC_LITERAL(11, 174, 16), // "showPointToolTip"
+QT_MOC_LITERAL(12, 191, 12), // "QMouseEvent*"
+QT_MOC_LITERAL(13, 204, 5), // "event"
+QT_MOC_LITERAL(14, 210, 13), // "SaveLastClick"
+QT_MOC_LITERAL(15, 224, 14), // "SaveLastClick2"
+QT_MOC_LITERAL(16, 239, 11), // "FindClosest"
+QT_MOC_LITERAL(17, 251, 7), // "double&"
+QT_MOC_LITERAL(18, 259, 1), // "a"
+QT_MOC_LITERAL(19, 261, 1), // "b"
+QT_MOC_LITERAL(20, 263, 15), // "QVector<double>"
+QT_MOC_LITERAL(21, 279, 3), // "_xx"
+QT_MOC_LITERAL(22, 283, 3), // "_yy"
+QT_MOC_LITERAL(23, 287, 4), // "flag"
+QT_MOC_LITERAL(24, 292, 4), // "int&"
+QT_MOC_LITERAL(25, 297, 6), // "_Index"
+QT_MOC_LITERAL(26, 304, 22), // "on_maxCheckBox_clicked"
+QT_MOC_LITERAL(27, 327, 22), // "on_minCheckBox_clicked"
+QT_MOC_LITERAL(28, 350, 27), // "on_TimeAdjustButton_clicked"
+QT_MOC_LITERAL(29, 378, 23), // "on_pushButton_4_clicked"
+QT_MOC_LITERAL(30, 402, 23) // "on_pushButton_3_clicked"
+
+    },
+    "MainWindow\0on_pushButton_clicked\0\0"
+    "on_pushButton_2_clicked\0"
+    "on_horizontalSlider_valueChanged\0value\0"
+    "on_horizontalSlider_2_valueChanged\0"
+    "xAxisChanged\0QCPRange\0range\0xAxisChanged2\0"
+    "showPointToolTip\0QMouseEvent*\0event\0"
+    "SaveLastClick\0SaveLastClick2\0FindClosest\0"
+    "double&\0a\0b\0QVector<double>\0_xx\0_yy\0"
+    "flag\0int&\0_Index\0on_maxCheckBox_clicked\0"
+    "on_minCheckBox_clicked\0"
+    "on_TimeAdjustButton_clicked\0"
+    "on_pushButton_4_clicked\0on_pushButton_3_clicked"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      15,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   89,    2, 0x08 /* Private */,
+       3,    0,   90,    2, 0x08 /* Private */,
+       4,    1,   91,    2, 0x08 /* Private */,
+       6,    1,   94,    2, 0x08 /* Private */,
+       7,    1,   97,    2, 0x08 /* Private */,
+      10,    1,  100,    2, 0x08 /* Private */,
+      11,    1,  103,    2, 0x08 /* Private */,
+      14,    1,  106,    2, 0x08 /* Private */,
+      15,    1,  109,    2, 0x08 /* Private */,
+      16,    6,  112,    2, 0x08 /* Private */,
+      26,    0,  125,    2, 0x08 /* Private */,
+      27,    0,  126,    2, 0x08 /* Private */,
+      28,    0,  127,    2, 0x08 /* Private */,
+      29,    0,  128,    2, 0x08 /* Private */,
+      30,    0,  129,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, 0x80000000 | 8,    9,
+    QMetaType::Void, 0x80000000 | 8,    9,
+    QMetaType::Void, 0x80000000 | 12,   13,
+    QMetaType::Void, 0x80000000 | 12,   13,
+    QMetaType::Void, 0x80000000 | 12,   13,
+    QMetaType::Void, 0x80000000 | 17, 0x80000000 | 17, 0x80000000 | 20, 0x80000000 | 20, QMetaType::Bool, 0x80000000 | 24,   18,   19,   21,   22,   23,   25,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        MainWindow *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButton_clicked(); break;
+        case 1: _t->on_pushButton_2_clicked(); break;
+        case 2: _t->on_horizontalSlider_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->on_horizontalSlider_2_valueChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 4: _t->xAxisChanged((*reinterpret_cast< QCPRange(*)>(_a[1]))); break;
+        case 5: _t->xAxisChanged2((*reinterpret_cast< QCPRange(*)>(_a[1]))); break;
+        case 6: _t->showPointToolTip((*reinterpret_cast< QMouseEvent*(*)>(_a[1]))); break;
+        case 7: _t->SaveLastClick((*reinterpret_cast< QMouseEvent*(*)>(_a[1]))); break;
+        case 8: _t->SaveLastClick2((*reinterpret_cast< QMouseEvent*(*)>(_a[1]))); break;
+        case 9: _t->FindClosest((*reinterpret_cast< double(*)>(_a[1])),(*reinterpret_cast< double(*)>(_a[2])),(*reinterpret_cast< QVector<double>(*)>(_a[3])),(*reinterpret_cast< QVector<double>(*)>(_a[4])),(*reinterpret_cast< bool(*)>(_a[5])),(*reinterpret_cast< int(*)>(_a[6]))); break;
+        case 10: _t->on_maxCheckBox_clicked(); break;
+        case 11: _t->on_minCheckBox_clicked(); break;
+        case 12: _t->on_TimeAdjustButton_clicked(); break;
+        case 13: _t->on_pushButton_4_clicked(); break;
+        case 14: _t->on_pushButton_3_clicked(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        switch (_id) {
+        default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+        case 9:
+            switch (*reinterpret_cast<int*>(_a[1])) {
+            default: *reinterpret_cast<int*>(_a[0]) = -1; break;
+            case 3:
+            case 2:
+                *reinterpret_cast<int*>(_a[0]) = qRegisterMetaType< QVector<double> >(); break;
+            }
+            break;
+        }
+    }
+}
+
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, Q_NULLPTR, Q_NULLPTR}
+};
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return Q_NULLPTR;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(const_cast< MainWindow*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 15)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 15;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 15)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 15;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
---
+++ readme.txt
@@ -0,0 +1,2 @@
+in order to run app, please download this content. There is exe file:
+https://drive.google.com/file/d/0BwnJdYUEG6V4cE9uQWhDM19iYUk/view?usp=sharing
